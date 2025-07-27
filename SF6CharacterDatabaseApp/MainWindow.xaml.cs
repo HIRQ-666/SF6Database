253983349cdeb3889415a84703b1a73afbf37e08
HIRQ <zhaitengheye966@gmail.com>
2025-07-28 00:11:12 +0900

    Fix Solutions

--- SF6CharacterDatabaseApp/MainWindow.xaml.cs
+++ SF6CharacterDatabaseApp/MainWindow.xaml.cs
@@ -9,7 +9,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WpfApp1
+namespace SF6CharacterDatabaseApp
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
