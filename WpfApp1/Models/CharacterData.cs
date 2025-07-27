705a24c6e89606ec4a8e2d4ee70771fcc2f16851
HIRQ <zhaitengheye966@gmail.com>
2025-07-27 22:48:10 +0900

    Create DataClasses

--- WpfApp1/Models/CharacterData.cs
+++ WpfApp1/Models/CharacterData.cs
@@ -3,7 +3,7 @@ public class CharacterData
     public string Name { get; set; } = string.Empty;
 
     public List<AttackData> Attacks { get; set; } = new();             // 技の情報
-    public string Strategy { get; set; } = string.Empty;               // 基本立ち回り
+    public List<StrategyPoint> Strategies { get; set; } = new();       // 基本立ち回り
     public List<ComboData> Combos { get; set; } = new();               // コンボ
-    public Dictionary<string, string> MatchupTips { get; set; } = new(); // 対キャラ対策
+    public List<MatchupData> Matchups { get; set; } = new();           // 対キャラ対策
 }
