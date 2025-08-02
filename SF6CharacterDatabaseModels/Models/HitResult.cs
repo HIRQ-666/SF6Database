aebc16e4cb2fb6e8141ada5f86e9d05ae611d138
HIRQ <zhaitengheye966@gmail.com>
2025-08-02 11:49:46 +0900

    Fix Hit Result

--- SF6CharacterDatabaseModels/Models/HitResult.cs
+++ SF6CharacterDatabaseModels/Models/HitResult.cs
@@ -4,7 +4,7 @@ namespace SF6CharacterDatabaseModels.Models
 {
     public class HitResult
     {
-        public int Frame { get; set; } = -1;
+        public int Frame { get; set; } = -999;
         public HitEffectType Effect { get; set; } = HitEffectType.None;
 
         public HitResult(int frame = -999, HitEffectType effect = HitEffectType.None)
